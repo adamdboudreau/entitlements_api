cdf291861f4ebc2609f5a8fb776ee67d86dd30f3
Sergey Skyba <sskyba@gmail.com>
2016-04-26 11:03:27 -0400

    Daily commit Apr 26, 2016

--- lib/connection.rb
+++ lib/connection.rb
@@ -6,6 +6,9 @@ class Connection
   attr_reader :connection
 
   def initialize
-    @connection = Cassandra.cluster(Cfg.config['cassandraCluster']).connect
+  	cluster = { hosts: Cfg.config['cassandraCluster']['hosts'], port: Cfg.config['cassandraCluster']['port'] }
+  	cluster[:username] = Cfg.config['cassandraCluster']['username'] unless Cfg.config['cassandraCluster']['username'].empty?
+  	cluster[:password] = Cfg.config['cassandraCluster']['password'] unless Cfg.config['cassandraCluster']['password'].empty?
+    @connection = Cassandra.cluster(cluster).connect
   end
 end
