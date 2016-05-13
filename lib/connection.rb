48f79b6f4e691f9dadb6088b4e0b52796326a036
Sergey Skyba <sskyba@gmail.com>
2016-05-13 14:09:15 -0400

    debugging

--- lib/connection.rb
+++ lib/connection.rb
@@ -40,76 +40,102 @@ class Connection
   end
 
   def getEntitlements(params, exclude_future_entitlements = true)
-    search_date = params['search_date'] ? Time.at(params['search_date'].to_i) : Time.now
-    cql = "SELECT source, product, trace_id, start_date, end_date FROM #{@table_entitlements} WHERE guid=? AND brand=? AND end_date>?"
-    args = [params['guid'], params['brand'], search_date]
     result = Array.new
-    @connection.execute(cql, arguments: args).each do |row|
-      result << row unless ((exclude_future_entitlements && (search_date<row['start_date'])) || 
-                            (params['source'] && row['source']!=params['source']) || 
-                            (params['product'] && row['product']!=params['product']) || 
-      	                    (params['trace_id'] && row['trace_id']!=params['trace_id'])
-      	                   )
-    end
+    begin
+      search_date = params['search_date'] ? Time.at(params['search_date'].to_i) : Time.now
+      cql = "SELECT source, product, trace_id, start_date, end_date FROM #{@table_entitlements} WHERE guid=? AND brand=? AND end_date>?"
+      args = [params['guid'], params['brand'], search_date]
+      @connection.execute(cql, arguments: args).each do |row|
+        result << row unless ((exclude_future_entitlements && (search_date<row['start_date'])) || 
+                              (params['source'] && row['source']!=params['source']) || 
+                              (params['product'] && row['product']!=params['product']) || 
+        	                    (params['trace_id'] && row['trace_id']!=params['trace_id'])
+        	                   )
+      end
+    rescue Exception => e
+      $logger.error "Connection.deleteEntitlements EXCEPTION: #{e.message}"
+      $logger.error "Connection.deleteEntitlements backtrace: #{e.backtrace.inspect}"
+    end  
     $logger.debug "\nConnection.getEntitlements, running CQL=#{cql} with args=#{args}, returning #{result.length} row(s)\n"
     result  
   end
 
   def deleteEntitlements(params)
     result = 0
-    batch = @connection.batch do |batch|
-      getEntitlements(params, false).each do |row|
-        cql1 = "DELETE FROM #{@table_entitlements} WHERE guid=? AND brand=? AND source=? AND product=? AND trace_id=? AND end_date=?"
-        cql2 = "INSERT INTO #{@table_history} (guid,brand,source,product,trace_id,end_date,start_date,archive_date,archive_type) VALUES (?,?,?,?,?,?,?,NOW(),'Deleted')"
-        args = Array[params['guid'],params['brand'],row['source'],row['product'],row['trace_id'],row['end_date']]
-        $logger.debug "Connection.deleteEntitlements, adding to batch: CQL=#{cql1} with arguments: #{args}}\n"
-        batch.add(cql1,args)
-        $logger.debug "Connection.deleteEntitlements, adding to batch: CQL=#{cql2} with arguments: #{args<<row['start_date']}}\n"
-        batch.add(cql2, args)
-        result += 1
+    begin
+      batch = @connection.batch do |batch|
+        getEntitlements(params, false).each do |row|
+          cql1 = "DELETE FROM #{@table_entitlements} WHERE guid=? AND brand=? AND source=? AND product=? AND trace_id=? AND end_date=?"
+          cql2 = "INSERT INTO #{@table_history} (guid,brand,source,product,trace_id,end_date,start_date,archive_date,archive_type) VALUES (?,?,?,?,?,?,?,NOW(),'Deleted')"
+          args = Array[params['guid'],params['brand'],row['source'],row['product'],row['trace_id'],row['end_date']]
+          $logger.debug "Connection.deleteEntitlements, adding to batch: CQL=#{cql1} with arguments: #{args}}\n"
+          batch.add(cql1,args)
+          $logger.debug "Connection.deleteEntitlements, adding to batch: CQL=#{cql2} with arguments: #{args<<row['start_date']}}\n"
+          batch.add(cql2, args)
+          result += 1
+        end
       end
-    end
-    @connection.execute(batch)
+      @connection.execute(batch)
+    rescue Exception => e
+      $logger.error "Connection.deleteEntitlements EXCEPTION: #{e.message}"
+      $logger.error "Connection.deleteEntitlements backtrace: #{e.backtrace.inspect}"
+    end  
     result
   end
 
   def archiveEntitlements()
     result = 0
     cql = "SELECT * FROM #{@table_entitlements_by_enddate} WHERE end_date<toTimestamp(NOW()) ALLOW FILTERING"
-    batch = @connection.batch do |batch|
-      @connection.execute(cql).each do |row|
-        args = Array[row['guid'],row['brand'],row['source'],row['product'],row['trace_id'],row['end_date']]
-        batch.add("DELETE FROM #{@table_entitlements} WHERE guid=? AND brand=? AND source=? AND product=? AND trace_id=? AND end_date=?",args)
-        batch.add("INSERT INTO #{@table_history} (guid,brand,source,product,trace_id,end_date,start_date,archive_date,archive_type) VALUES (?,?,?,?,?,?,?,NOW(),'Cleanup')", args<<row['start_date'])
-        result += 1
+    begin
+      batch = @connection.batch do |batch|
+        @connection.execute(cql).each do |row|
+          args = Array[row['guid'],row['brand'],row['source'],row['product'],row['trace_id'],row['end_date']]
+          batch.add("DELETE FROM #{@table_entitlements} WHERE guid=? AND brand=? AND source=? AND product=? AND trace_id=? AND end_date=?",args)
+          batch.add("INSERT INTO #{@table_history} (guid,brand,source,product,trace_id,end_date,start_date,archive_date,archive_type) VALUES (?,?,?,?,?,?,?,NOW(),'Cleanup')", args<<row['start_date'])
+          result += 1
+        end
       end
-    end
-    @connection.execute(batch)
-    $logger.debug "\nConnection.archiveEntitlements, running CQL=#{cql}, returns #{result.to_s}\n"
+      @connection.execute(batch)
+      $logger.debug "\nConnection.archiveEntitlements, running CQL=#{cql}, returns #{result.to_s}\n"
+    rescue Exception => e
+      $logger.error "Connection.archiveEntitlements EXCEPTION: #{e.message}"
+      $logger.error "Connection.archiveEntitlements backtrace: #{e.backtrace.inspect}"
+    end  
     result
   end
 
   def getTC(params)
     result = nil
     if params['guid'] && (Cfg.config['brands'].include? params['brand'])
-      cql = "SELECT * FROM #{@table_tc} WHERE guid=? AND brand=? LIMIT 1"
-      args = [params['guid'], params['brand']]
-      $logger.debug "\nConnection.getTC, running CQL=#{cql} with args=#{args}\n"
-      @connection.execute(cql, arguments: args).each do |row|
-        result = { version: row['tc_version'], acceptance_date: row['tc_acceptance_date'] }
-      end 
+      begin
+        cql = "SELECT * FROM #{@table_tc} WHERE guid=? AND brand=? LIMIT 1"
+        args = [params['guid'], params['brand']]
+        $logger.debug "\nConnection.getTC, running CQL=#{cql} with args=#{args}\n"
+        @connection.execute(cql, arguments: args).each do |row|
+          result = { version: row['tc_version'], acceptance_date: row['tc_acceptance_date'] }
+        end 
+      rescue Exception => e
+        $logger.error "Connection.getTC EXCEPTION: #{e.message}"
+        $logger.error "Connection.getTC backtrace: #{e.backtrace.inspect}"
+      end  
     end
     result  
   end
 
   def putTC(params)
-    cql = "UPDATE #{@table_tc} SET tc_acceptance_date=toTimestamp(now()), tc_version=? WHERE guid=? AND brand=?"
-    args = [params['tc_version'], params['guid'], params['brand']]
-    $logger.debug "\nConnection.putTC, running CQL=#{cql} with args=#{args}\n"
-    statement = @connection.prepare(cql)
-    @connection.execute(statement, arguments: args)
-    true
-  end
+    begin
+      cql = "UPDATE #{@table_tc} SET tc_acceptance_date=toTimestamp(now()), tc_version=? WHERE guid=? AND brand=?"
+      args = [params['tc_version'], params['guid'], params['brand']]
+      $logger.debug "\nConnection.putTC, running CQL=#{cql} with args=#{args}\n"
+      statement = @connection.prepare(cql)
+      @connection.execute(statement, arguments: args)
+      true
+    rescue Exception => e  
+      $logger.error "Connection.putTC EXCEPTION: #{e.message}"
+      $logger.error "Connection.putTC backtrace: #{e.backtrace.inspect}"
+      false
+    end  
+   end
 
   def runCQL(params)
   	result = Array.new
@@ -120,8 +146,8 @@ class Connection
       end 
       $logger.debug "Connection.runCQL finished ok, result=#{result.to_s}"
     rescue Exception => e  
-      $logger.debug "Connection.runCQL EXCEPTION: #{e.message}"
-      $logger.debug "Connection.runCQL backtrace: #{e.backtrace.inspect}"
+      $logger.error "Connection.runCQL EXCEPTION: #{e.message}"
+      $logger.error "Connection.runCQL backtrace: #{e.backtrace.inspect}"
     end  
     result
   end
