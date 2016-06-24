94adad6d2b1a99ac78ebacedd66e339d1176ae9d
Sergey Skyba <sskyba@gmail.com>
2016-06-24 13:07:46 -0400

    Debugging

--- helpers/camp.rb
+++ helpers/camp.rb
@@ -3,7 +3,7 @@ class CAMP
   def check (guid)
     $logger.debug "\nCAMP.check started\n"
 
-    $logger.debug "OpenSSL settings: " + OpenSSL.inspect
+    $logger.debug "OpenSSL settings: " + OpenSSL::SSL.inspect
 
     uri = URI.parse(Cfg.config['campAPI']['url'] + guid)
     $logger.debug "\nCAMP.check going to ping URL=#{uri}\n"
