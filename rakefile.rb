efe215f7a3ac0f7e7553a98fef254364c0cd5a6a
Sergey Skyba <sskyba@gmail.com>
2017-06-06 12:27:22 -0400

    PROD! Datetime format change for Sylvan's cron job

--- rakefile.rb
+++ rakefile.rb
@@ -515,13 +515,15 @@ task :backup, [:table_name] do |task, args|
 
   File.open(sFileName, "w") do |f|
     f.write (args[:table_name] ? "guid,brand,tc_acceptance_date,tc_version\n" : "guid,brand,end_date,source,product,trace_id,start_date\n")
+#    f.write ("guid,brand,end_date,source,product,trace_id,start_date\n")
     result  = @connection.execute("SELECT * FROM #{sTableName}", page_size: 1000)
     loop do
       result.each do |row|
         if args[:table_name]
-          f.write "#{row['guid']},#{row['brand']},#{row['tc_acceptance_date']},#{row['tc_version']}\n"
+          f.write "#{row['guid']},#{row['brand']},#{row['tc_acceptance_date'].strftime("%F %T")},#{row['tc_version']}\n"
+#          f.write "#{row['guid']},#{row['brand']},#{row['end_date'].strftime("%F %T")},#{row['source']},#{row['product']},#{row['trace_id']},#{row['start_date'].strftime("%F %T")}\n"
         else
-          f.write "#{row['guid']},#{row['brand']},#{row['end_date']},#{row['source']},#{row['product']},#{row['trace_id']},#{row['start_date']}\n"
+          f.write "#{row['guid']},#{row['brand']},#{row['end_date'].strftime("%F %T")},#{row['source']},#{row['product']},#{row['trace_id']},#{row['start_date'].strftime("%F %T")}\n"
         end
         nCounter += 1
       end
@@ -579,10 +581,10 @@ task :backupDelta, [:table_name] do |task, args|
   File.open(sFileName, "w") do |f|
     result = nil
     if args[:table_name]=='tc'
-      f.write ("guid,brand,tc_acceptance_date_timestamp,tc_version\n")
+      f.write ("guid,brand,tc_acceptance_date,tc_version\n")
       result = @connection.execute("SELECT guid,brand,tc_acceptance_date,toUnixTimestamp(tc_acceptance_date) AS tc_acceptance_date_timestamp,tc_version FROM #{sTableName}", page_size: 1000)
     else
-      f.write ("guid,brand,source,product,trace_id,start_date_timestamp,end_date\n")
+      f.write ("guid,brand,source,product,trace_id,start_date,end_date\n")
       result = @connection.execute("SELECT guid,brand,source,product,trace_id,start_date,toUnixTimestamp(start_date) AS start_date_timestamp,end_date FROM #{sTableName}", page_size: 1000)
     end
 
@@ -590,10 +592,10 @@ task :backupDelta, [:table_name] do |task, args|
       result.each do |row|
         nTotal += 1
         if (args[:table_name]=='tc') && (row['tc_acceptance_date_timestamp']>start_date) && (row['tc_acceptance_date_timestamp']<end_date)
-          f.write "#{row['guid']},#{row['brand']},#{row['tc_acceptance_date']},#{row['tc_version']}\n"
+          f.write "#{row['guid']},#{row['brand']},#{row['tc_acceptance_date'].strftime("%F %T") },#{row['tc_version']}\n"
           nFound += 1
         elsif row['start_date'] && (row['start_date_timestamp']>start_date) && (row['start_date_timestamp']<end_date)
-          f.write "#{row['guid']},#{row['brand']},#{row['source']},#{row['product']},#{row['trace_id']},#{row['start_date']},#{row['end_date']}\n"
+          f.write "#{row['guid']},#{row['brand']},#{row['source']},#{row['product']},#{row['trace_id']},#{row['start_date'].strftime("%F %T")},#{row['end_date'].strftime("%F %T")}\n"
           nFound += 1
         end
       end
