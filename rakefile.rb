f4e4e024de531ad635ac2d2ce54abfb97b02eb34
Sergey Skyba <sskyba@gmail.com>
2017-06-23 15:31:02 -0400

    cassandra_test rake task added

--- rakefile.rb
+++ rakefile.rb
@@ -840,31 +840,22 @@ desc 'cassandra_test'
 task :cassandra_test do
   start_date_min = (Time.now-24.hours).beginning_of_day.strftime("%F %T%z")
   start_date_max = (Time.now-24.hours).end_of_day.strftime("%F %T%z")
-  raResults = Array.new
-  cql = "SELECT COUNT(*) FROM #{Cfg.config['tables']['entitlements']} WHERE start_date>='#{start_date_min}' AND start_date<='#{start_date_max}' ALLOW FILTERING"
+  nFound = 0
+  cql = "SELECT * FROM #{Cfg.config['cassandraCluster']['keyspace']}.#{Cfg.config['tables']['entitlements']} WHERE start_date>='#{start_date_min}' AND start_date<='#{start_date_max}' ALLOW FILTERING"
   puts "cassandra_test :: Cassandra integrity test task started, CQL to test: #{cql}"
-  Cfg.config['cassandraCluster']['hosts'].each do |host|
-    puts "cassandra_test :: Trying host #{host}"
-    cluster = { hosts: [host], port: Cfg.config['cassandraCluster']['port'], timeout: 30 }
-    cluster[:username] = Cfg.config['cassandraCluster']['username'] unless Cfg.config['cassandraCluster']['username'].empty?
-    cluster[:password] = Cfg.config['cassandraCluster']['password'] unless Cfg.config['cassandraCluster']['password'].empty?
-    if Cfg.config['cassandraCluster']['use_ssl']
-      cluster[:server_cert] = Cfg.config['cassandraCluster']['certServer']
-      cluster[:client_cert] = Cfg.config['cassandraCluster']['certClient']
-      cluster[:private_key] = Cfg.config['cassandraCluster']['certKey']
-      cluster[:passphrase] = ENV['CASSANDRA_PASSPHRASE']
+  begin
+    @connection ||= Connection.instance.connection
+    result = @connection.execute(cql, consistency: :all, page_size: 1000)
+    loop do
+      result.each do |row|
+        nFound += 1
+        puts "#{nFound} :: #{row}"
+      end
+      break if result.last_page?
+      result = result.next_page
     end
-
-    begin
-      puts "cassandra_test :: Going to connect to cluster #{cluster}"
-      session = Cassandra.cluster(cluster).connect Cfg.config['cassandraCluster']['keyspace']
-      puts "cassandra_test :: Connected to #{host}"
-#      result = session.execute("USE #{Cfg.config['cassandraCluster']['keyspace']}")
-#      puts "cassandra_test :: keyspace changed ok to #{Cfg.config['cassandraCluster']['keyspace']}"
-      result = session.execute(cql, timeout: 30)
-      puts "cassandra_test :: Got result: #{result.length}"
-    rescue Exception => e
-      puts "cassandra_test :: ERROR! EXCEPTION: #{e.message}\nBacktrace: #{e.backtrace.inspect}"
-    end
+    puts "cassandra_test :: finished ok for #{nFound} records"
+  rescue Exception => e
+    puts "cassandra_test :: ERROR! EXCEPTION: #{e.message}\nBacktrace: #{e.backtrace.inspect}"
   end
 end
